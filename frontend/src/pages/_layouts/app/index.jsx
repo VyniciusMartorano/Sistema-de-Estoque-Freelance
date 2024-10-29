16a7c39f9ab036aaaec07e6fedeb18a8bfbe2eca
VyniciusMartorano <vyniciussalusto@gmail.com>
2024-10-29 20:01:00 -0300

    menu responsivo

--- frontend/src/pages/_layouts/app/index.jsx
+++ frontend/src/pages/_layouts/app/index.jsx
@@ -1,4 +1,6 @@
-import { Menubar } from 'primereact/menubar'
+import './index.css'
+
+import { MegaMenu } from 'primereact/megamenu'
 import { PanelMenu } from 'primereact/panelmenu'
 import { Sidebar } from 'primereact/sidebar'
 import { useContext, useState } from 'react'
@@ -27,6 +29,12 @@ export function AppLayout() {
   const { navigate } = useSGCNavigate()
   const userLogged = `${user?.first_name ?? ''} ${user?.last_name ?? ''}`
 
+  const end = (
+    <div className="flex flex-row">
+      <SGCAvatar isMale={isMale} userName={userLogged} />
+    </div>
+  )
+
   if (!isAuthenticated) {
     return <Navigate to="/login" />
   }
@@ -40,68 +48,78 @@ export function AppLayout() {
   }
 
   return (
-    <div className="relative h-screen  ">
-      <div
-        className={`fixed left-0 right-0 top-0 z-50 flex w-full flex-row items-center justify-between gap-4 bg-sgc-blue-background-light pr-5 ${isLoading && 'blur-xl'} ${visible ? 'pl-[296px]' : 'pl-[116px]'}`}
-      >
-        <ButtonToggleSidebar handleOpenSidebar={handleOpenSidebar} />
-        <div className="flex flex-row">
-          <HeaderIconsContainer />
+    <div>
+      <div className="menu-desktop relative  h-screen">
+        <div
+          className={`fixed left-0 right-0 top-0 z-50 flex w-full flex-row items-center justify-between gap-4 bg-sgc-blue-background-light pr-5 ${isLoading && 'blur-xl'} ${visible ? 'pl-[296px]' : 'pl-[116px]'}`}
+        >
+          <ButtonToggleSidebar handleOpenSidebar={handleOpenSidebar} />
+          <div className="menu-desktop flex flex-row">
+            <HeaderIconsContainer />
 
-          <SGCAvatar isMale={isMale} userName={userLogged} />
+            <SGCAvatar isMale={isMale} userName={userLogged} />
+          </div>
+        </div>
+
+        {isLoading && (
+          <div className="absolute z-[9999999] flex h-screen w-screen items-center justify-center bg-black bg-opacity-50 ">
+            <img alt="Logo Simas" className="w-52 animate-bounce" src={logo} />
+          </div>
+        )}
+
+        <Sidebar
+          visible
+          dismissable={false}
+          onHide={() => setVisible((prev) => !prev)}
+          showCloseIcon={false}
+          modal={false}
+          header={<HeaderSidebar />}
+          className={`${isLoading && 'blur-xl'} menu-desktop`}
+          style={{
+            width: visible ? 280 : 100,
+            transition: 'width 0.2s ease-in-out',
+          }}
+        >
+          <div className="mt-4 flex flex-col gap-2">
+            <PanelMenu
+              model={menus?.map((el) => {
+                // Correção temporária: removendo o prefixo 'fas fa-' dos ícones do font-awesome
+                el.icon = el.icon.replace('fas fa-', '')
+                if (el.items) {
+                  el.items = el.items.map((item) => {
+                    item.icon = item.icon.replace('fas fa-', '')
+
+                    if (item.items) {
+                      item.items = item.items.map((subItem) => {
+                        subItem.icon = subItem.icon.replace('fas fa-', '')
+                        return subItem
+                      })
+                    }
+                    return item
+                  })
+                }
+                return el
+              })}
+              className={` ${!visible ? 'hideLabelItems' : 'removePadding'}`}
+            />
+          </div>
+        </Sidebar>
+
+        <div
+          className={`${visible ? 'pl-[296px]' : 'pl-[116px]'} p-4 pt-16 ${isLoading ? 'blur-xl' : ''}`}
+        >
+          <Outlet />
         </div>
       </div>
 
-      {isLoading && (
-        <div className="absolute z-[9999999] flex h-screen w-screen items-center justify-center bg-black bg-opacity-50 ">
-          <img alt="Logo Simas" className="w-52 animate-bounce" src={logo} />
-        </div>
-      )}
-      <Menubar id="menu-mobile" model={menus}></Menubar>
-
-      <Sidebar
-        id="menu-desktop"
-        visible
-        dismissable={false}
-        onHide={() => setVisible((prev) => !prev)}
-        showCloseIcon={false}
-        modal={false}
-        header={<HeaderSidebar />}
-        className={`${isLoading && 'blur-xl'}`}
-        style={{
-          width: visible ? 280 : 100,
-          transition: 'width 0.2s ease-in-out',
-        }}
-      >
-        <div className="mt-4 flex flex-col gap-2">
-          <PanelMenu
-            model={menus?.map((el) => {
-              // Correção temporária: removendo o prefixo 'fas fa-' dos ícones do font-awesome
-              el.icon = el.icon.replace('fas fa-', '')
-              if (el.items) {
-                el.items = el.items.map((item) => {
-                  item.icon = item.icon.replace('fas fa-', '')
-
-                  if (item.items) {
-                    item.items = item.items.map((subItem) => {
-                      subItem.icon = subItem.icon.replace('fas fa-', '')
-                      return subItem
-                    })
-                  }
-                  return item
-                })
-              }
-              return el
-            })}
-            className={` ${!visible ? 'hideLabelItems' : 'removePadding'}`}
-          />
-        </div>
-      </Sidebar>
-
-      <div
-        className={`${visible ? 'pl-[296px]' : 'pl-[116px]'} p-4 pt-16 ${isLoading ? 'blur-xl' : ''}`}
-      >
-        <Outlet />
+      <div className="menu-mobile ">
+        <MegaMenu
+          model={menus}
+          orientation="horizontal"
+          end={end}
+          breakpoint="960px"
+          className="surface-0 shadow-2 rounded-none bg-sgc-blue-background-light p-3"
+        />
       </div>
     </div>
   )
