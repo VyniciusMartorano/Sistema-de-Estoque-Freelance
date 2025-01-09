c653bc4a8c3f2177777b3c2eec5218e36a12bd21
VyniciusMartorano <vyniciussalusto@gmail.com>
2025-01-09 20:36:32 -0300

    ajuste

--- frontend/src/context/service.js
+++ frontend/src/context/service.js
@@ -1,28 +1,28 @@
-import { apiBase } from '@/api/apibase'
+import { apiBase } from "@/api/apiBase";
 
 class Service {
   getUser() {
-    return apiBase.axios.get(`/user/`)
+    return apiBase.axios.get(`/user/`);
   }
 
   getMenus() {
-    return apiBase.axios.get(`/menuitem/`)
+    return apiBase.axios.get(`/menuitem/`);
   }
 
   async getPermissions() {
-    return await apiBase.axios.get('permissions/')
+    return await apiBase.axios.get("permissions/");
   }
 
   getUserRegistered(username) {
-    return apiBase.axios.get(`/get_user/${username}`)
+    return apiBase.axios.get(`/get_user/${username}`);
   }
 
   async signIn(payload) {
-    return await apiBase.axios.post(`/token/`, payload)
+    return await apiBase.axios.post(`/token/`, payload);
   }
 
   changeUserPassword(payload) {
-    return apiBase.axios.post(`/reset_passworrd/`, payload)
+    return apiBase.axios.post(`/reset_passworrd/`, payload);
   }
 }
-export default Service
+export default Service;
